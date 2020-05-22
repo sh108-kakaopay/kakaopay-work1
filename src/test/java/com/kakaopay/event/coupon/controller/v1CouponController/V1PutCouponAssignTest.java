2f921b1de5000dca9b370ca62eb1c3a5675374a8
yangs1202 <me@yangs.kr>
2020-05-22 23:41:09 +0900

    쿠폰 할당 API 스펙 변경 POST -> PUT

--- src/test/java/com/kakaopay/event/coupon/controller/v1CouponController/V1PutCouponAssignTest.java
+++ src/test/java/com/kakaopay/event/coupon/controller/v1CouponController/V1PutCouponAssignTest.java
@@ -8,7 +8,6 @@ import com.kakaopay.event.coupon.domain.enums.CouponErrorStatus;
 import com.kakaopay.event.coupon.domain.enums.CouponStatus;
 import com.kakaopay.event.coupon.domain.response.V1CouponErrorResponse;
 import com.kakaopay.event.coupon.repository.CouponRepository;
-import com.kakaopay.event.coupon.util.MockMvcTestUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -26,17 +24,15 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(
-        properties = {"propertyTest.value=propertyTest", "testValue=test"}
-)
+@SpringBootTest
 @AutoConfigureMockMvc
 @Slf4j
-@DisplayName("[쿠폰 할당 API] [POST] /v1/coupon/assign TEST")
-class V1PostAssignCouponTest {
+@DisplayName("[쿠폰 할당 API] [PUT] /v1/coupon/{serial}/assign TEST")
+class V1PutCouponAssignTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -44,7 +40,7 @@ class V1PostAssignCouponTest {
     private CouponRepository couponRepository;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final String url = "/v1/coupon/assign";
+    private final String url = "/v1/coupon/%s/assign";
 
     @BeforeEach
     void setup() {
@@ -59,11 +55,8 @@ class V1PostAssignCouponTest {
     @Test
     @DisplayName("[E] 없는 쿠폰을 요청 했을때 에러")
     void 없는_쿠폰을_요청_했을때_에러() throws Exception {
-        MvcResult result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(MockMvcTestUtil.buildUrlEncodedFormEntity(
-                        "coupon-serial", UUID.randomUUID().toString()
-                ))
+        MvcResult result = mockMvc.perform(
+                put(String.format(url, UUID.randomUUID().toString()))
         ).andExpect(
                 status().isBadRequest()
         ).andDo(
@@ -89,11 +82,8 @@ class V1PostAssignCouponTest {
 
         couponRepository.saveAndFlush(coupon);
 
-        MvcResult result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(MockMvcTestUtil.buildUrlEncodedFormEntity(
-                        "coupon-serial", targetCouponSerial
-                ))
+        MvcResult result = mockMvc.perform(
+                put(String.format(url, targetCouponSerial))
         ).andExpect(
                 status().isBadRequest()
         ).andDo(
@@ -118,11 +108,8 @@ class V1PostAssignCouponTest {
 
         couponRepository.saveAndFlush(coupon);
 
-        MvcResult result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(MockMvcTestUtil.buildUrlEncodedFormEntity(
-                        "coupon-serial", targetCouponSerial
-                ))
+        MvcResult result = mockMvc.perform(
+                put(String.format(url, targetCouponSerial))
         ).andExpect(
                 status().isBadRequest()
         ).andDo(
@@ -137,11 +124,8 @@ class V1PostAssignCouponTest {
     @Test
     @DisplayName("[E] 36자리에 맞춰서 요청하지 않았을경우")
     void 시리얼을_36자리에_맞춰서_요청하지_않았을경우() throws Exception {
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(MockMvcTestUtil.buildUrlEncodedFormEntity(
-                        "coupon-serial", "111"
-                ))
+        mockMvc.perform(
+                put(String.format(url, UUID.randomUUID().toString() + "1"))
         ).andExpect(
                 status().isBadRequest()
         ).andDo(
@@ -164,11 +148,8 @@ class V1PostAssignCouponTest {
         coupon.setRegTimestamp(LocalDateTime.MAX);
         couponRepository.saveAndFlush(coupon);
 
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(MockMvcTestUtil.buildUrlEncodedFormEntity(
-                        "coupon-serial", targetCouponSerial
-                ))
+        mockMvc.perform(
+                put(String.format(url, targetCouponSerial))
         ).andExpect(
                 status().isOk()
         ).andDo(
