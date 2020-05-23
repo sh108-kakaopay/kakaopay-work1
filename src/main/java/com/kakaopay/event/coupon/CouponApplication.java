aa2e7598c8070c85d02342f8464e986573cffc71
yangs1202 <me@yangs.kr>
2020-05-24 01:57:31 +0900

    메세지 발송 관련 배치 기능 추가

--- src/main/java/com/kakaopay/event/coupon/CouponApplication.java
+++ src/main/java/com/kakaopay/event/coupon/CouponApplication.java
@@ -1,14 +1,20 @@
 package com.kakaopay.event.coupon;
 
+import com.kakaopay.event.coupon.config.AppConfig;
+import com.kakaopay.event.coupon.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableConfigurationProperties({JwtConfig.class, AppConfig.class})
 @SpringBootApplication
 public class CouponApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(CouponApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/kakaopay/event/coupon/batch/ExpiredUserBatch.java
@@ -0,0 +1,31 @@
+package com.kakaopay.event.coupon.batch;
+
+import com.kakaopay.event.coupon.config.AppConfig;
+import com.kakaopay.event.coupon.service.CouponService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+@Slf4j
+public class ExpiredUserBatch {
+    private final AppConfig appConfig;
+    private final CouponService couponService;
+
+
+    @Scheduled(cron = "00 09 * * *")
+    public void run() {
+        if (appConfig.isBatchEnable() == false) {
+            return;
+        }
+
+        if (couponService.sendExpiredMessage(appConfig.getBatchTargetDay()) == false) {
+            log.info(String.format("Success Send Expired Message (target)  : %d", appConfig.getBatchTargetDay()));
+        } else {
+            log.info(String.format("Failed Send Expired Message (target)  : %d", appConfig.getBatchTargetDay()));
+        }
+
+    }
+}
